a875a37bce61de596a7d70216869a13498f0b70a
pumpk <cong2738@gmail.com>
2025-04-29 13:26:36 +0900

    APB 인터페이스 신호 및 모듈 구조 개선: PSEL 및 PRDATA 배열로 변경, 불필요한 신호 제거

--- my_RISC-V.srcs/sources_1/APB_Master.sv
+++ my_RISC-V.srcs/sources_1/APB_Master.sv
@@ -9,44 +9,24 @@ module APB_Master (
     output logic [31:0] PWDATA,
     output logic        PWRITE,
     output logic        PENABLE,
-    output logic        PSEL0,
-    output logic        PSEL1,
-    output logic        PSEL2,
-    output logic        PSEL3,
-    output logic        PSEL4,
-    output logic        PSEL5,
-    input  logic [31:0] PRDATA0,
-    input  logic [31:0] PRDATA1,
-    input  logic [31:0] PRDATA2,
-    input  logic [31:0] PRDATA3,
-    input  logic [31:0] PRDATA4,
-    input  logic [31:0] PRDATA5,
-    input  logic        PREADY0,
-    input  logic        PREADY1,
-    input  logic        PREADY2,
-    input  logic        PREADY3,
-    input  logic        PREADY4,
-    input  logic        PREADY5,
+    output logic [15:0] PSEL,
+    input  logic [31:0] PRDATA  [0:15],
+    input  logic [15:0] PREADY,
     // Internal Interface Signals
-    input  logic        transfer,  // trigger signal
+    input  logic        transfer,        // trigger signal
     output logic        ready,
     input  logic [31:0] addr,
     input  logic [31:0] wdata,
     output logic [31:0] rdata,
-    input  logic        write      // 1:write, 0:read
+    input  logic        write            // 1:write, 0:read
 );
     logic [31:0] temp_addr_next, temp_addr_reg;
     logic [31:0] temp_wdata_next, temp_wdata_reg;
     logic temp_write_next, temp_write_reg;
     logic decoder_en;
-    logic [5:0] pselx;
+    logic [15:0] pselx;
 
-    assign PSEL0 = pselx[0];
-    assign PSEL1 = pselx[1];
-    assign PSEL2 = pselx[2];
-    assign PSEL3 = pselx[3];
-    assign PSEL4 = pselx[4];
-    assign PSEL5 = pselx[5];
+    assign PSEL = pselx;
 
     typedef enum bit [1:0] {
         IDLE,
@@ -85,7 +65,7 @@ module APB_Master (
                 decoder_en = 1'b0;
                 if (transfer) begin
                     state_next      = SETUP;
-                    temp_addr_next  = addr;  // latching
+                    temp_addr_next  = addr;  
                     temp_wdata_next = wdata;
                     temp_write_next = write;
                 end
@@ -127,18 +107,8 @@ module APB_Master (
 
     APB_Mux U_APB_Mux (
         .sel  (temp_addr_reg),
-        .d0   (PRDATA0),
-        .d1   (PRDATA1),
-        .d2   (PRDATA2),
-        .d3   (PRDATA3),
-        .d4   (PRDATA4),
-        .d5   (PRDATA5),
-        .r0   (PREADY0),
-        .r1   (PREADY1),
-        .r2   (PREADY2),
-        .r3   (PREADY3),
-        .r4   (PREADY4),
-        .r5   (PREADY5),
+        .d   (PRDATA),
+        .r   (PREADY),
         .rdata(rdata),
         .ready(ready)
     );
@@ -147,18 +117,28 @@ endmodule
 module APB_Decoder (
     input  logic        en,
     input  logic [31:0] sel,
-    output logic [ 5:0] y
+    output logic [15:0] y
 );
     always_comb begin
-        y = 6'b0;
+        y = 16'b0;
         if (en) begin
             casex (sel)
-                32'h1000_0xxx: y = 6'b000001;
-                32'h1000_1xxx: y = 6'b000010;
-                32'h1000_2xxx: y = 6'b000100;
-                32'h1000_3xxx: y = 6'b001000;
-                32'h1000_4xxx: y = 6'b010000;
-                32'h1000_5xxx: y = 6'b100000;
+                32'h1000_0xxx: y[0]  = 1;
+                32'h1000_1xxx: y[1]  = 1;
+                32'h1000_2xxx: y[2]  = 1;
+                32'h1000_3xxx: y[3]  = 1;
+                32'h1000_4xxx: y[4]  = 1;
+                32'h1000_5xxx: y[5]  = 1;
+                32'h1000_6xxx: y[6]  = 1;
+                32'h1000_7xxx: y[7]  = 1;
+                32'h1000_8xxx: y[8]  = 1;
+                32'h1000_9xxx: y[9]  = 1;
+                32'h1000_axxx: y[10] = 1;
+                32'h1000_bxxx: y[11] = 1;
+                32'h1000_cxxx: y[12] = 1;
+                32'h1000_dxxx: y[13] = 1;
+                32'h1000_exxx: y[14] = 1;
+                32'h1000_fxxx: y[15] = 1;
             endcase
         end
     end
@@ -166,18 +146,8 @@ endmodule
 
 module APB_Mux (
     input  logic [31:0] sel,
-    input  logic [31:0] d0,
-    input  logic [31:0] d1,
-    input  logic [31:0] d2,
-    input  logic [31:0] d3,
-    input  logic [31:0] d4,
-    input  logic [31:0] d5,
-    input  logic        r0,
-    input  logic        r1,
-    input  logic        r2,
-    input  logic        r3,
-    input  logic        r4,
-    input  logic        r5,
+    input  logic [31:0] d    [0:15],
+    input  logic [15:0] r,
     output logic [31:0] rdata,
     output logic        ready
 );
@@ -185,24 +155,44 @@ module APB_Mux (
     always_comb begin
         rdata = 32'bx;
         casex (sel)
-            32'h1000_0xxx: rdata = d0;
-            32'h1000_1xxx: rdata = d1;
-            32'h1000_2xxx: rdata = d2;
-            32'h1000_3xxx: rdata = d3;
-            32'h1000_4xxx: rdata = d4;
-            32'h1000_5xxx: rdata = d5;
+            32'h1000_0xxx: rdata = d[0];
+            32'h1000_1xxx: rdata = d[1];
+            32'h1000_2xxx: rdata = d[2];
+            32'h1000_3xxx: rdata = d[3];
+            32'h1000_4xxx: rdata = d[4];
+            32'h1000_5xxx: rdata = d[5];
+            32'h1000_6xxx: rdata = d[6];
+            32'h1000_7xxx: rdata = d[7];
+            32'h1000_8xxx: rdata = d[8];
+            32'h1000_9xxx: rdata = d[9];
+            32'h1000_axxx: rdata = d[10];
+            32'h1000_bxxx: rdata = d[11];
+            32'h1000_cxxx: rdata = d[12];
+            32'h1000_dxxx: rdata = d[13];
+            32'h1000_exxx: rdata = d[14];
+            32'h1000_fxxx: rdata = d[15];
         endcase
     end
 
     always_comb begin
         ready = 1'b0;
         casex (sel)
-            32'h1000_0xxx: ready = r0;
-            32'h1000_1xxx: ready = r1;
-            32'h1000_2xxx: ready = r2;
-            32'h1000_3xxx: ready = r3;
-            32'h1000_4xxx: ready = r4;
-            32'h1000_5xxx: ready = r5;
+            32'h1000_0xxx: ready = r[0];
+            32'h1000_1xxx: ready = r[1];
+            32'h1000_2xxx: ready = r[2];
+            32'h1000_3xxx: ready = r[3];
+            32'h1000_4xxx: ready = r[4];
+            32'h1000_5xxx: ready = r[5];
+            32'h1000_6xxx: ready = r[6];
+            32'h1000_7xxx: ready = r[7];
+            32'h1000_8xxx: ready = r[8];
+            32'h1000_9xxx: ready = r[9];
+            32'h1000_axxx: ready = r[10];
+            32'h1000_bxxx: ready = r[11];
+            32'h1000_cxxx: ready = r[12];
+            32'h1000_dxxx: ready = r[13];
+            32'h1000_exxx: ready = r[14];
+            32'h1000_fxxx: ready = r[15];
         endcase
     end
 endmodule
--- my_RISC-V.srcs/sources_1/MCU.sv
+++ my_RISC-V.srcs/sources_1/MCU.sv
@@ -3,54 +3,33 @@
 module MCU (
     input  logic       clk,
     input  logic       reset,
-    output logic [7:0] GPOA,
-    input  logic [7:0] GPIB,
     inout  logic [7:0] GPIOC,
-    inout  logic [7:0] GPIOD,
+    inout  logic [3:0] GPIOD,
     output logic [3:0] fndCom,
     output logic [7:0] fndFont
 );
-    // global signals
     logic        PCLK;
     logic        PRESET;
-    // APB Interface Signals
+
     logic [31:0] PADDR;
     logic [31:0] PWDATA;
     logic        PWRITE;
     logic        PENABLE;
-    logic        PSEL_RAM;
-    logic        PSEL_GPO;
-    logic        PSEL_GPI;
-    logic        PSEL_GPIOC;
-    logic        PSEL_GPIOD;
-    logic        PSEL_FND;
-    logic [31:0] PRDATA_RAM;
-    logic [31:0] PRDATA_GPO;
-    logic [31:0] PRDATA_GPI;
-    logic [31:0] PRDATA_GPIOC;
-    logic [31:0] PRDATA_GPIOD;
-    logic [31:0] PRDATA_FND;
-    logic        PREADY_RAM;
-    logic        PREADY_GPO;
-    logic        PREADY_GPI;
-    logic        PREADY_GPIOC;
-    logic        PREADY_GPIOD;
-    logic        PREADY_FND;
+    logic [15:0] PSEL;
+    logic [31:0] PRDATA       [0:15];
+    logic [15:0] PREADY;
 
-    // CPU - APB_Master Signals
-    // Internal Interface Signals
-    logic        transfer;  // trigger signal
+    logic        transfer;
     logic        ready;
     logic [31:0] addr;
     logic [31:0] wdata;
     logic [31:0] rdata;
-    logic        write;  // 1:write, 0:read
+    logic        write;
     logic        dataWe;
     logic [31:0] dataAddr;
     logic [31:0] dataWData;
     logic [31:0] dataRData;
 
-    // ROM Signals
     logic [31:0] instrCode;
     logic [31:0] instrMemAddr;
 
@@ -68,79 +47,54 @@ module MCU (
 
     RV32I_Core U_Core (.*);
 
-    APB_Master U_APB_Master (
-        .*,
-        .PSEL0  (PSEL_RAM),
-        .PSEL1  (PSEL_GPO),
-        .PSEL2  (PSEL_GPI),
-        .PSEL3  (PSEL_GPIOC),
-        .PSEL4  (PSEL_GPIOD),
-        .PSEL5  (PSEL_FND),
-        .PRDATA0(PRDATA_RAM),
-        .PRDATA1(PRDATA_GPO),
-        .PRDATA2(PRDATA_GPI),
-        .PRDATA3(PRDATA_GPIOC),
-        .PRDATA4(PRDATA_GPIOD),
-        .PRDATA5(PRDATA_FND),
-        .PREADY0(PREADY_RAM),
-        .PREADY1(PREADY_GPO),
-        .PREADY2(PREADY_GPI),
-        .PREADY3(PREADY_GPIOC),
-        .PREADY4(PREADY_GPIOD),
-        .PREADY5(PREADY_FND)
-    );
+    APB_Master U_APB_Master (.*);
 
     ram U_RAM (
         .*,
-        .PSEL  (PSEL_RAM),
-        .PRDATA(PRDATA_RAM),
-        .PREADY(PREADY_RAM)
+        .PSEL  (PSEL[0]),
+        .PRDATA(PRDATA[0]),
+        .PREADY(PREADY[0])
     );
 
-    GPO_Periph U_GPOA (
+    GPIO_Periph U_GPOA (
         .*,
-        .PSEL   (PSEL_GPO),
-        .PRDATA (PRDATA_GPO),
-        .PREADY (PREADY_GPO),
-        // export signals
-        .outPort(GPOA)
+        .PSEL(PSEL[1]),
+        .PRDATA(PRDATA[1]),
+        .PREADY(PREADY[1]),
+        .inoutPort(GPIOA)
     );
 
-    GPI_Periph U_GPIB (
+    GPIO_Periph U_GPIB (
         .*,
-        .PSEL  (PSEL_GPI),
-        .PRDATA(PRDATA_GPI),
-        .PREADY(PREADY_GPI),
-        // inport signals
-        .inPort(GPIB)
+        .PSEL  (PSEL[2]),
+        .PRDATA(PRDATA[2]),
+        .PREADY(PREADY[2]),
+        .inoutPort(GPIOB)
     );
 
     GPIO_Periph U_GPIOC (
         .*,
-        .PSEL     (PSEL_GPIOC),
-        .PRDATA   (PRDATA_GPIOC),
-        .PREADY   (PREADY_GPIOC),
-        // inoutport signals
+        .PSEL(PSEL[3]),
+        .PRDATA(PRDATA[3]),
+        .PREADY(PREADY[3]),
         .inoutPort(GPIOC)
     );
 
     GPIO_Periph U_GPIOD (
         .*,
-        .PSEL     (PSEL_GPIOD),
-        .PRDATA   (PRDATA_GPIOD),
-        .PREADY   (PREADY_GPIOD),
-        // inoutport signals
+        .PSEL(PSEL[4]),
+        .PRDATA(PRDATA[4]),
+        .PREADY(PREADY[4]),
         .inoutPort(GPIOD)
     );
 
     fnd_Periph u_fnd_pp (
         .*,
-        .PSEL(PSEL_FND),
-        .PRDATA(PRDATA_FND),
-        .PREADY(PREADY_FND),
+        .PSEL   (PSEL[5]),
+        .PRDATA (PRDATA[5]),
+        .PREADY (PREADY[5]),
         .fndFont(fndFont),
-        .fndCom(fndCom),
-        .sim_dp(),
-        .sim_bcd()
+        .fndCom (fndCom)
     );
+
 endmodule
