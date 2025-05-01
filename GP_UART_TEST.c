89bad280ad0c26c87abe6f8dbb36d8d12248e749
pumpk <cong2738@gmail.com>
2025-05-02 01:38:34 +0900

    GPUART 테스트 코드

---
+++ GP_UART_TEST.c
@@ -0,0 +1,139 @@
+#include <stdint.h>
+
+#define __IO             volatile
+
+typedef struct{
+    __IO uint32_t MODER;
+    __IO uint32_t ODR;
+} GPO_TypeDef;
+
+typedef struct{
+    __IO uint32_t MODER;
+    __IO uint32_t IDR;
+} GPI_TypeDef;
+
+typedef struct{
+    __IO uint32_t MODER;
+    __IO uint32_t IDR;
+    __IO uint32_t ODR;
+} GPIO_TypeDef;
+
+typedef struct{
+    __IO uint32_t FCR;
+    __IO uint32_t FDR;
+    __IO uint32_t DP;
+} GPFND_TypeDef;
+
+typedef struct{
+    __IO uint32_t TCR;
+    __IO uint32_t TCNT;
+    __IO uint32_t PSC;
+    __IO uint32_t ARR;
+} TIMER_TypeDef;
+
+typedef struct{
+    __IO uint32_t TXD;
+    __IO uint32_t RXD;
+    __IO uint32_t tx_full;
+    __IO uint32_t rx_empty;
+} GPUART_TypeDef;
+
+#define APB_BASEADDR    0x10000000
+#define GPOA_BASEADDR   (APB_BASEADDR + 0x1000)
+#define GPIB_BASEADDR   (APB_BASEADDR + 0x2000)
+#define GPIOC_BASEADDR  (APB_BASEADDR + 0x3000)
+#define GPIOD_BASEADDR  (APB_BASEADDR + 0x4000)
+#define GPFND_BASEADDR  (APB_BASEADDR + 0x5000)
+#define GPUART_BASEADDR  (APB_BASEADDR + 0x5000)
+
+#define GPOA            ((GPO_TypeDef *) GPOA_BASEADDR)
+#define GPIB            ((GPI_TypeDef *) GPIB_BASEADDR)
+#define GPIOC           ((GPIO_TypeDef *) GPIOC_BASEADDR)
+#define GPIOD           ((GPIO_TypeDef *) GPIOD_BASEADDR)
+#define GPFND           ((GPFND_TypeDef *) GPFND_BASEADDR)
+#define GPUART           ((GPUART_TypeDef *) GPUART_BASEADDR)
+
+void delay(int n);
+
+void LED_init(GPIO_TypeDef *GPIOx);
+void LED_write(GPIO_TypeDef *GPIOx, uint32_t data);
+
+void Switch_init(GPIO_TypeDef *GPIOx);
+uint32_t Switch_read(GPIO_TypeDef *GPIOx);
+
+void FND_init(GPFND_TypeDef *fnd, uint32_t ON_OFF);
+void FND_writeData(GPFND_TypeDef *fnd, uint32_t data, uint32_t dp);
+
+uint32_t UART_isFUll(GPUART_TypeDef* UART);
+uint32_t UART_isEMPTY(GPUART_TypeDef* UART);
+void UART_trans(GPUART_TypeDef* UART, uint32_t data);
+uint32_t UART_read(GPUART_TypeDef* UART);
+
+int main(void)
+{
+    uint32_t readData = 0;
+    while (1)
+    {
+        if(!UART_isEMPTY(GPUART)) readData = UART_read(GPUART);
+        if(!UART_isFUll(GPUART)) if(readData) UART_trans(GPUART, readData);
+    }
+    
+    return 0;
+}
+
+void delay(int n)
+{
+    uint32_t temp = 0;
+    for (int i=0; i<n; i++){
+        for (int j=0; j<1000; j++){
+            temp++;
+        }
+    }
+}
+
+void LED_init(GPIO_TypeDef *GPIOx)
+{
+    GPIOx->MODER = 0xff;
+}
+
+void LED_write(GPIO_TypeDef *GPIOx, uint32_t data)
+{
+    GPIOx->ODR = data;
+}
+
+void Switch_init(GPIO_TypeDef *GPIOx)
+{
+    GPIOx->MODER = 0x00;
+}
+
+uint32_t Switch_read(GPIO_TypeDef *GPIOx)
+{
+    return GPIOx->IDR;
+}
+
+void FND_init(GPFND_TypeDef *fnd, uint32_t ON_OFF)
+{
+    fnd->FCR = ON_OFF;
+}
+
+void FND_writeData(GPFND_TypeDef *fnd, uint32_t data, uint32_t dp)
+{
+    fnd->FDR = data;
+    fnd->DP = dp;
+}
+
+uint32_t UART_isFUll(GPUART_TypeDef* UART) {
+    return UART->tx_full;
+}
+
+uint32_t UART_isEMPTY(GPUART_TypeDef* UART) {
+    return UART->rx_empty;
+}
+
+void UART_trans(GPUART_TypeDef* UART, uint32_t data) {
+    UART->TXD = data;
+}
+
+uint32_t UART_read(GPUART_TypeDef* UART) {
+    return UART->RXD;
+}
