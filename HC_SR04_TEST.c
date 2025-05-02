96ca9a37c1148e4e62b5da98573d31734a10e9b1
pumpk <cong2738@gmail.com>
2025-05-03 00:03:34 +0900

    테스트코드

--- HC_SR04_TEST.c
+++ HC_SR04_TEST.c
@@ -42,10 +42,12 @@ typedef struct{
     __IO uint32_t IDR;
 } HCSR04_TypeDef;
 
+#define APB_ADDR_GAP    0x1000
+
 #define APB_BASEADDR    0x10000000
-#define GPFND_BASEADDR  (APB_BASEADDR + 0x1000)
-#define GPUART_BASEADDR (APB_BASEADDR + 0x2000)
-#define HCSR04_BASEADDR (APB_BASEADDR + 0x3000)
+#define GPFND_BASEADDR  (APB_BASEADDR + APB_ADDR_GAP)
+#define GPUART_BASEADDR (GPFND_BASEADDR + APB_ADDR_GAP)
+#define HCSR04_BASEADDR (GPUART_BASEADDR + APB_ADDR_GAP)
 
 #define GPFND           ((GPFND_TypeDef *) GPFND_BASEADDR)
 #define GPUART          ((GPUART_TypeDef *) GPUART_BASEADDR)
@@ -71,10 +73,13 @@ uint32_t HCSR04_READ(HCSR04_TypeDef* hc_sr04);
 int main(void)
 {
     FND_init(GPFND, 1);
-    uint32_t readData ='x';
+    uint32_t readData = 0;
     while (1)
-    {
-        FND_writeData(GPFND, HCSR04_READ(HCSR04), 0xf);
+    {  
+        readData = UART_read(GPUART);
+        if(readData != 0) UART_trans(GPUART, readData);
+        FND_writeData(GPFND, HCSR04_READ(HCSR04),0xf);
+        delay(1000);
     }
     
     return 0;
