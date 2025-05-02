0c0a556b280437ef2b28a63d9f7116b2d785aec4
pumpk <cong2738@gmail.com>
2025-05-03 00:49:19 +0900

    tb_HCSR04 모듈 생성

---
+++ my_RISC-V.srcs/sim_1/new/tb_HCSR04.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2025/05/03 00:48:42
+// Design Name: 
+// Module Name: tb_HCSR04
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_HCSR04(
+
+    );
+endmodule
