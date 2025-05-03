950837d3fcfb4db2eb52294135a059befd733f19
pumpk <cong2738@gmail.com>
2025-05-04 01:43:07 +0900

    거리센서 제네럴페리페럴 완성   - 버퍼없음, 일반 레지스터 사용   - 출력: 10진수 거리 정수 데이터

--- my_RISC-V.srcs/sources_1/clk_devider/tick_generator.sv
+++ my_RISC-V.srcs/sources_1/clk_devider/tick_generator.sv
@@ -1,16 +1,16 @@
 `timescale 1ns / 1ps
 
 module tick_generator #(parameter MAX_COUNT = 100_000_000)(
-    input logic PCLK,
-    input logic PRESET,
+    input logic clk,
+    input logic reset,
     output logic tick
 );
     logic [31:0] count_num;
-    always_ff @(posedge PCLK, posedge PRESET) begin : blockName
-        if (PRESET) begin
+    always_ff @(posedge clk, posedge reset) begin : blockName
+        if (reset) begin
             count_num <= 0;
             tick <= 0;
-        end else if (PCLK) begin
+        end else if (clk) begin
             if (count_num == MAX_COUNT - 1) begin
                 count_num <= 0;
                 tick <= 1;
