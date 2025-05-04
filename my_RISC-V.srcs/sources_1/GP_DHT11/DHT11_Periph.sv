6f796b10159f166cc55b4087494aed8395728146
pumpk <cong2738@gmail.com>
2025-05-04 21:19:37 +0900

    feat: DHT11 모듈 추가

--- my_RISC-V.srcs/sources_1/GP_DHT11/DHT11_Periph.sv
+++ my_RISC-V.srcs/sources_1/GP_DHT11/DHT11_Periph.sv
@@ -13,45 +13,25 @@ module DHT11_Periph (
     input  logic        PSEL,
     output logic [31:0] PRDATA,
     output logic        PREADY,
-    inout  logic        DATA_IO,
-    // 검증용
-    output logic [7:0] sim_rh,
-    output logic [7:0] sim_t,
-    output logic        sim_finish 
+    inout  logic        DATA_IO
 );
 
-    logic [7:0] rh_int, t_int;
-    logic finish_int;
+    logic [15:0] rh, rh_p, t, t_p;
 
-    assign sim_rh  = rh_int;  // 시뮬레이션을 위한 assign
-    assign sim_t = t_int;  // 시뮬레이션을 위한 assign
-    assign sim_finish = finish_int;  // 시뮬레이션을 위한 assign
+    APB_SlaveIntf_DHT11 U_APB_Intf (.*);
 
-  APB_SlaveIntf_DHT11 U_APB_Intf (
-      .PCLK      (PCLK),
-      .PRESET    (PRESET),
-      .PADDR     (PADDR),
-      .PWDATA    (PWDATA),
-      .PWRITE    (PWRITE),
-      .PENABLE   (PENABLE),
-      .PSEL      (PSEL),
-      .PRDATA    (PRDATA),
-      .PREADY    (PREADY),
-      .rh_int    (rh_int),
-      .t_int     (t_int),
-      .finish_int(finish_int)
-  );
-  
-  DHT11_module U_DHT11_IP (
-      .clk          (PCLK),
-      .reset        (PRESET),
-      .data         (DATA_IO),
-      .rh           (rh_int),
-      .t            (t_int),
-      .w_finish_tick(finish_int)
-  );
+    DHT11_module U_DHT11_IP (
+        .clk          (PCLK),
+        .reset        (PRESET),
+        .data         (DATA_IO),
+        .rh           (rh),
+        .rh_p           (rh_p),
+        .t            (t),
+        .t_p            (t_p),
+        .w_finish_tick()
+    );
 endmodule
- 
+
 module APB_SlaveIntf_DHT11 (
     // global signal
     input  logic        PCLK,
@@ -64,53 +44,33 @@ module APB_SlaveIntf_DHT11 (
     input  logic        PSEL,
     output logic [31:0] PRDATA,
     output logic        PREADY,
-    input  logic [ 7:0] rh_int,
-    input  logic [ 7:0] t_int,
-    input  logic        finish_int
-
+    input  logic [15:0] rh,
+    input  logic [15:0] rh_p,
+    input  logic [15:0] t,
+    input  logic [15:0] t_p
 );
-  
 
-  logic [31:0] slv_reg0, slv_reg1, slv_reg2, slv_reg3;
 
-//   assign slv_reg0[7:0] = rh_int;
-//   assign slv_reg1[7:0] = t_int;
-//   assign slv_reg2[0] = finish_int;
+    logic [31:0] slv_reg0, slv_reg1, slv_reg2, slv_reg3;
 
-  assign slv_reg0 = {24'd0, rh_int};
-  assign slv_reg1 = {24'd0, t_int};
-  assign slv_reg2 = {31'd0, finish_int};
-  
+    assign slv_reg0 = rh;
+    assign slv_reg1 = rh_p;
+    assign slv_reg2 = t;
+    assign slv_reg3 = t_p;
 
-  always_ff @(posedge PCLK, posedge PRESET) begin
+    always_ff @(posedge PCLK, posedge PRESET) begin
         if (PRESET) begin
-            //slv_reg0 <= 0;
-            //slv_reg1 <= 0;
-            //slv_reg2 <= 0;
-            // slv_reg3 <= 0;
-            // PREADY <= 1'b0;
-            // PRDATA <= 32'd0;
+            PREADY <= 0;
         end else begin
             if (PSEL && PENABLE) begin
                 PREADY <= 1'b1;
-                if (PWRITE) begin
-                    case (PADDR[3:2])
-                        2'd0: ;
-                        2'd1: ;
-                        2'd2: ;
-                        // 2'd3: ;
-                        // 2'd3: slv_reg3 <= PWDATA;
-                    endcase
-                end else begin
-                    // PRDATA <= 32'bx;
-                    PRDATA <= 32'd0; //수정
+                if (!PWRITE) begin
+                    PRDATA <= 32'd0;  //수정
                     case (PADDR[3:2])
                         2'd0: PRDATA <= slv_reg0;
                         2'd1: PRDATA <= slv_reg1;
-                        2'd2: PRDATA <= slv_reg2;
-                        // 2'd3: PRDATA <= slv_reg3;
-                        // 2'd3: PRDATA <= slv_reg3;
-                        // default: PRDATA <= 32'd0; //수정
+                        2'd1: PRDATA <= slv_reg2;
+                        2'd1: PRDATA <= slv_reg3;
                     endcase
                 end
             end else begin
